6ecdfcaff5ed83b9781115e9fa7ec7bac40d394b
Juan Solorzano <solojcarlos@gmail.com>
2026-08-11 15:53:51 -0500

    fix: hacer dinamica la pagina de perfil

--- src/app/admin/mis-cuyes/perfil/page.tsx
+++ src/app/admin/mis-cuyes/perfil/page.tsx
@@ -1,8 +1,14 @@
+export const dynamic = "force-dynamic";
+
 import { getAdminProfileData } from "@/features/mis-cuyes/profile/services/profile.service";
 import ProfileClient from "./ProfileClient";
 
 export default async function PerfilPage() {
   const profile = await getAdminProfileData();
 
-  return <ProfileClient initialProfile={profile} />;
+  return (
+    <ProfileClient
+      initialProfile={profile}
+    />
+  );
 }
